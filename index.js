9be20731c9e545eaacf9487619b380adfc88cc3a
MUSF71 <marcus.linu@gmail.com>
2024-09-16 16:43:45 -0300

    final 1

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
 let vitoria =200
-let derrotas = 80
 
- SaldoVitorias(vitoria, derrotas)
+let derrotas = 200
+
+  let saldo = SaldoVitorias(vitoria, derrotas)
+
+ if(saldo < 10){
+    console.log("O Herói tem de saldo de " + saldo + " está no nível de Ferro");
+
+}       else if (saldo >10 && saldo <= 20)  {
+   console.log("O Herói tem de saldo de " + saldo +" está no nível de Bronze ");
+
+   }   else if (saldo > 21 && saldo <= 50)  {
+       console.log("O Herói tem de saldo de " + saldo +" está no nível de Prata");
+
+   }    else if (saldo > 51 && saldo <= 80)  {
+       console.log("O Herói tem de saldo de " + saldo +" está no nível de Ouro");
+
+   }    else if (saldo > 81 && saldo <= 90)  {
+       console.log("O Herói tem de saldo de " + saldo +" está no nível de Diamante");
+
+   }   else if (saldo > 91 && saldo <= 100)  {
+       console.log("O Herói tem de saldo de " + saldo +" está no nível de Lendario");  
+
+   }   else { 
+          console.log("O Herói tem de saldo de " + saldo +" está no nível de Imortal")  
+   }  
 
 function SaldoVitorias(vitoria,derrotas) { 
-  let Num1=vitoria-derrotas;
+  var Num1=vitoria - derrotas
   return Num1
 }
 
-if(SaldoVitorias < 10){
-     console.log("O Herói tem de saldo de " + SaldoVitorias +" está no nível de Ferro");
 
-}       else if (SaldoVitorias >10 && SaldoVitorias < 20)  {
-    console.log("O Herói tem de saldo de " + SaldoVitorias +" está no nível de Bronze ");
-
-    }   else if (SaldoVitorias > 21 && SaldoVitorias < 50)  {
-        console.log("O Herói tem de saldo de " + SaldoVitorias +" está no nível de Prata");
-
-    }    else if (SaldoVitorias > 51 && SaldoVitorias < 80)  {
-        console.log("O Herói tem de saldo de " + SaldoVitorias +" está no nível de Ouro");
-
-    }    else if (SaldoVitorias > 81 && SaldoVitorias < 90)  {
-        console.log("O Herói tem de saldo de " + SaldoVitorias +" está no nível de Diamante");
-
-    }   else if (SaldoVitorias > 91 && SaldoVitorias < 100)  {
-        console.log("O Herói tem de saldo de " + SaldoVitorias +" está no nível de Lendario");  
-
-    }   else { 
-           console.log("O Herói tem de saldo de " + SaldoVitorias +" está no nível de Imortal")  
-    }  
  
